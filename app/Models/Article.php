eeed3aaf010a5e80cbeb886d3cd0d3b3cb684825
Yunio808 <reaper808@outlook.com>
2023-06-08 16:53:24 +0200

    'Zit-vast'

---
+++ app/Models/Article.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AppModels;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'excerpt', 'body'];
+}
